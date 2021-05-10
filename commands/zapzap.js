447261233218e9f37064ae41a9023420508c9778
aatz1 <joaocougi@gmail.com>
2021-05-10 16:09:13 -0300

    .

---
+++ commands/zapzap.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'zapzap',
+	description: 'Entra no canal de voz e toca audio ( zapzap.mp3 )',
+	async execute(message) {
+        if(message.member.voice.channel) {
+            
+        }
+			
+	}
+};
