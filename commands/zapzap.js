879f7cfc05d6bb4e32c8a2c22e460cfafa0df593
aatz1 <joaocougi@gmail.com>
2021-05-11 09:34:44 -0300

    Add zapzap command

--- commands/zapzap.js
+++ commands/zapzap.js
@@ -1,10 +1,20 @@
+const path = require('path')
+
+
 module.exports = {
 	name: 'zapzap',
 	description: 'Entra no canal de voz e toca audio ( zapzap.mp3 )',
 	async execute(message) {
-        if(message.member.voice.channel) {
-            
-        }
-			
+		const { voice } = message.member
+		
+		if(!voice.channelID) {
+			message.reply('Voce precisa estar em um canal de voz para estourar seus ouvidos')
+			return
+		} 
+		
+		voice.channel.join().then((connection) => {
+			connection.play(path.join(__dirname, 'zapzap.mp3'))
+		})
+		
 	}
 };
