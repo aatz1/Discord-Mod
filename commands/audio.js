12c1e66b0670bc4b6ac7ee232ead6977d3b9f37d
aatz1 <joaocougi@gmail.com>
2021-05-12 13:54:06 -0300

    Add audio command and gooood audios

--- commands/audio.js
+++ commands/audio.js
@@ -2,19 +2,22 @@ const path = require('path')
 
 
 module.exports = {
-	name: 'zapzap',
-	description: 'Entra no canal de voz e toca audio ( zapzap.mp3 )',
-	async execute(message) {
+	name: 'audio',
+	description: 'Entra no canal de voz e toca o nome do audio passado como argumento',
+	async execute(message, args) {
 		const { voice } = message.member
 		
 		if(!voice.channelID) {
 			message.reply('Voce precisa estar em um canal de voz para estourar seus ouvidos')
 			return
 		} 
+
+
+		const audioname = args[0]
 		
 		voice.channel.join().then((connection) => {
-			connection.play(path.join(__dirname, '../audios/zapzap.mp3'))
-			})
+			connection.play(path.join(__dirname, `../audios/${audioname}.mp3`))
+		})
 		
 	}
 };
